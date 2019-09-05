658eadf92149938ce989692d18c4c64ef02fd339
xiaoxiaobai2 <778998010@qq.com>
2019-09-05 20:00:52 +0800

    2019.09.05  封装方法，获取数据类型文件

--- Demo04_UDP/Client.java
+++ Demo04_UDP/Client.java
@@ -19,9 +19,43 @@ public class Client {
         //1、创建客户端 +端口
         DatagramSocket client=new DatagramSocket(8888);
         //2、准备数据
-        String msg="UDP协议";
+        String path="C:\\Users\\Administrator\\Desktop\\Test\\码农真苦.txt";
+        double d=3.14;
+        byte[] bytes=convert(d);
+
+        //3、打包数据
+        DatagramPacket packet=new DatagramPacket(bytes,bytes.length,new InetSocketAddress(InetAddress.getLocalHost(),6666));
+        //4、发送数据
+        client.send(packet);
+        //5、释放
+        client.close();
+    }
+    /**
+      * @author:  Zhang
+      * @description:
+      *
+      * 将 double类型的数据转换为字节流
+     **/
+    private static byte[] convert(Double d) throws IOException {
         byte[] bytes;
-        InputStream is=new BufferedInputStream(new FileInputStream("C:\\Users\\Administrator\\Desktop\\Test\\码农真苦.txt"));
+        ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+        DataOutputStream dps=new DataOutputStream(new BufferedOutputStream(byteArrayOutputStream));
+        dps.writeDouble(d);
+        dps.flush();
+        bytes=byteArrayOutputStream.toByteArray();
+        dps.close();
+        byteArrayOutputStream.close();
+        return bytes;
+    }
+    /**
+      * @author:  Zhang
+      * @description:
+      *
+      * 将文件内容转换 为字节流
+     **/
+    private static byte[] convert(String path) throws IOException {
+        byte[] bytes;
+        InputStream is=new BufferedInputStream(new FileInputStream(path));
         ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
         byte[] car=new byte[1024];
         int len =0;
@@ -29,15 +63,10 @@ public class Client {
             byteArrayOutputStream.write(car,0,len);
         }
         bytes=byteArrayOutputStream.toByteArray();
-        System.out.println(new String(bytes));
+//        System.out.println(new String(bytes));
         byteArrayOutputStream.flush();
         is.close();
         byteArrayOutputStream.close();
-        //3、打包数据
-        DatagramPacket packet=new DatagramPacket(bytes,bytes.length,new InetSocketAddress(InetAddress.getLocalHost(),6666));
-        //4、发送数据
-        client.send(packet);
-        //5、释放
-        client.close();
+        return bytes;
     }
 }
--- Demo04_UDP/Server.java
+++ Demo04_UDP/Server.java
@@ -1,9 +1,6 @@
 package Demo04_UDP;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
@@ -31,12 +28,34 @@ public class Server {
         server.receive(packet);
         //5、获取分析数据
         bytes=packet.getData();
-        int length=packet.getLength();
+        get_data(bytes);
+
+        //6、释放
+        server.close();
+    }
+    /**
+      * @author:  Zhang
+      * @description:
+      *
+      * 获取 将文件已字节流的形式传输的数据
+     **/
+    private static void get_File(byte[] bytes) throws IOException {
         InputStream is=new BufferedInputStream(new ByteArrayInputStream(bytes));
         bytes=is.readAllBytes();
         String s=new String(bytes,0,bytes.length);
+        is.close();
         System.out.println(s);
-        //6、释放
-        server.close();
+    }
+    /**
+      * @author:  Zhang
+      * @description:
+      *
+      * 获取 将数据以字节流的形式传输的数据
+     **/
+    private static void get_data(byte[] bytes) throws IOException {
+        DataInputStream dataInputStream=new DataInputStream(new BufferedInputStream(new ByteArrayInputStream(bytes)));
+        double d=dataInputStream.readDouble();
+        dataInputStream.close();
+        System.out.printf("double数据：%-6.2f",d);//%-m.n  加-号，左对齐，共占m位，小数占n位
     }
 }
